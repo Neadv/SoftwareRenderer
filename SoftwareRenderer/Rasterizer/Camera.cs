bbfaf8be193b7764efa60718914ffe5cdff1c016
Vladislav Krinitskiy <vlkrinitskiy@gmail.com>
2022-01-07 19:53:44 +0200

    Fix small issue

--- SoftwareRenderer/Rasterizer/Camera.cs
+++ SoftwareRenderer/Rasterizer/Camera.cs
@@ -15,13 +15,14 @@ namespace SoftwareRenderer.Rasterizer
             Position = position;
             Orientation = orientation;
 
+            float s2 = 1 / MathF.Sqrt(2);
             ClippingPlanes = new List<Plane>
             {
-                new Plane(new Vector3f(0, 0, 1), -1),                           // Near
-                new Plane(new Vector3f(MathF.Sqrt(2), 0, MathF.Sqrt(2)), 0),    // Left
-                new Plane(new Vector3f(-MathF.Sqrt(2), 0, MathF.Sqrt(2)), 0),   // Right
-                new Plane(new Vector3f(0, -MathF.Sqrt(2), MathF.Sqrt(2)), 0),   // Top
-                new Plane(new Vector3f(0, MathF.Sqrt(2), MathF.Sqrt(2)), 0)     // Bottom
+                new Plane(new Vector3f(0, 0, 1), -1),     // Near
+                new Plane(new Vector3f(s2, 0, s2), 0),    // Left
+                new Plane(new Vector3f(-s2, 0, s2), 0),   // Right
+                new Plane(new Vector3f(0, -s2, s2), 0),   // Top
+                new Plane(new Vector3f(0, s2, s2), 0)     // Bottom
             };
         }
 
